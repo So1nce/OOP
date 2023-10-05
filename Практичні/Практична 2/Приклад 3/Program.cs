abb2f3dc969bba6395f0a6153c1d394c2240ae02
So1nce <gorbulasasa@gmail.com>
2023-10-05 23:52:32 +0300

    changes

---
+++ Практичні/Практична 2/Приклад 3/Program.cs
@@ -0,0 +1,11 @@
+﻿string s = "бiологiя алгебра iсторiя географiя геометрiя";
+string c = "i";
+string[] a;
+a = s.Split(' ');
+int i;
+for(i = 0; i < a.Length; i++)
+{
+    int t = a[i].IndexOf(c);
+    if (t != -1) Console.WriteLine(a[i]);
+}
+Console.ReadKey();
---
+++ Практичні/Практична 2/Приклад 4/Program.cs
@@ -0,0 +1,7 @@
+﻿string s1 = "бiологiя алгебра iсторiя географiя геометрiя";
+string[] a;
+a = s1.Split(' ');
+Array.Reverse(a);
+string s2 = string.Join(" ", a);
+Console.WriteLine(s2);
+Console.ReadKey();
