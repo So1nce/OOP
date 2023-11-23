d145ed52eafa4df2c5137e25b51f491fa01c8e14
So1nce <gorbulasasa@gmail.com>
2023-11-24 01:49:22 +0200

    Done

---
+++ Лабораторні/Лабораторна 3/Індивідуальне завдання/Самостiйна робота/XmlParse.cs
@@ -0,0 +1,68 @@
+﻿using System.Xml;
+
+public class XmlParse
+{
+    public static Notepad ParseNotepad(string xmlString)
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(xmlString);
+
+        Notepad notepad = new Notepad();
+
+        foreach (XmlNode noteNode in xmlDoc.SelectNodes("/notepad/note"))
+        {
+            int id = int.Parse(noteNode.Attributes["id"].Value);
+            string date = noteNode.Attributes["date"].Value;
+            string time = noteNode.Attributes["time"].Value;
+            string subject = noteNode.SelectSingleNode("subject").InnerText;
+            string text = noteNode.SelectSingleNode("text").InnerText.Trim();
+            string tel = noteNode.SelectSingleNode("text/tel")?.InnerText;
+
+            Note note = new Note
+            {
+                Id = id,
+                Date = date,
+                Time = time,
+                Subject = subject,
+                Text = text,
+                Tel = tel
+            };
+
+            notepad.AddNote(note);
+        }
+
+        return notepad;
+    }
+
+    public static Log ParseLog(string xmlString)
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(xmlString);
+
+        Log log = new Log();
+
+        foreach (XmlNode eventNode in xmlDoc.SelectNodes("/log/event"))
+        {
+            string date = eventNode.Attributes["date"].Value;
+            string result = eventNode.Attributes["result"].Value;
+            string ipFrom = eventNode.SelectSingleNode("ip-from").InnerText.Trim();
+            string method = eventNode.SelectSingleNode("method").InnerText.Trim();
+            string urlTo = eventNode.SelectSingleNode("url-to").InnerText.Trim();
+            int response = int.Parse(eventNode.SelectSingleNode("response").InnerText);
+
+            Event logEvent = new Event
+            {
+                Date = date,
+                Result = result,
+                IpFrom = ipFrom,
+                Method = method,
+                UrlTo = urlTo,
+                Response = response
+            };
+
+            log.AddEvent(logEvent);
+        }
+
+        return log;
+    }
+}
---
+++ Лабораторні/Лабораторна 3/Приклад/Cars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Приклад
+{
+    internal class Cars
+    {
+        public static List<Shop> CarsList = new List<Shop> ();
+        public static void Add(Shop s)
+        {
+            CarsList.Add (s);
+        }
+        public static void Show()
+        {
+            foreach(Shop i in CarsList) { i.PrintToConsole(); }
+        }
+    }
+}
